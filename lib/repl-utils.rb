ec0cea43d52cf33e9a3d70f004d50cb82ab507e8
Matt Baker <mbaker.pdx@gmail.com>
2014-07-19 11:00:22 -0700

    Comment and augment "example!"

--- lib/repl-utils.rb
+++ lib/repl-utils.rb
@@ -1,10 +1,17 @@
 module ReplUtils
   def example!
+    #Demonstrate object creation and referencing by a variable
     @greeting = Example::String.new("Hello")
+    #Demonstrate two variables referencing the same object
     @english_greeting = @greeting
+    #Demonstrate object references
     @greetings = Example::Array.new
     @greetings << @greeting
+    #Demonstrate object references to an "un-named" object
     @greetings << Example::String.new("Hola")
+    #Demonstrate an orphaned object
+    #Force ruby's garbage collection with GC.start to watch it disappear!
+    Example::String.new("Hard knock life")
     nil
   end
 
